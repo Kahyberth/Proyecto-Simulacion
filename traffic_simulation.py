f747a74635b4177ef106c6946a541887eb925ba8
Kahyberth <kahyberth.gonzalez@correounivalle.edu.co>
2023-12-18 15:23:19 -0500

    new traffic_simulation

--- traffic_simulation.py
+++ traffic_simulation.py
@@ -15,13 +15,19 @@ class TrafficSimulation:
         self.car_position_lane2 = -width // 2
         self.semaphore_interval = 8000
         self.last_semaphore_change = pygame.time.get_ticks() # Tiempo en milisegundos
-        self.is_green_left = True
-        self.is_green_right = False
+        self.is_green_left = False
+        self.is_green_right = True
+        self.is_green_top = False
+        self.is_green_bottom = False
         self.lane1_cars = []
         self.lane2_cars = []
+        self.lane3_cars = []
+        self.lane4_cars = []
         self.car_generation_chance = 0.2
         self.traffic_light_left = TrafficLight(self.screen, 300, 130 )
-        self.traffic_light_right = TrafficLight(self.screen, 700, 130)
+        self.traffic_light_right = TrafficLight(self.screen, 700, 325)
+        self.traffic_light_top = TrafficLight(self.screen, 300, 325)
+        self.traffic_light_bottom = TrafficLight(self.screen, 700, 130)
         self.font = pygame.font.Font(None, 36)
         self.restart_button = Button(screen, 100, height - 120, 120, 50, (0, 0, 255), "Restart", self.restart_game)
         self.pause_resume_button = Button(screen, 250, height - 120, 180, 50, (255, 165, 0), "Pause/Resume", self.toggle_pause)
@@ -43,9 +49,12 @@ class TrafficSimulation:
             self.update_traffic_lights()
             self.generate_cars()
         self.screen.fill((255, 255, 255))
-        self.draw_bridge(self.height // 2 - 100)
+        self.draw_bridge(0, self.height // 2 - 100, is_vertical=False)
+        self.draw_bridge(self.width // 2 - 50, 0, is_vertical=True)
         self.traffic_light_left.change_color(self.is_green_left)
         self.traffic_light_right.change_color(self.is_green_right)
+        self.traffic_light_top.change_color(self.is_green_top)
+        self.traffic_light_bottom.change_color(self.is_green_bottom)
         self.draw_cars()
         self.draw_traffic_light_status()
         self.draw_instructions()
@@ -61,7 +70,15 @@ class TrafficSimulation:
             f"Right Light: {'Green' if self.is_green_right else 'Red'}",
             True, (0, 0, 0)
         )
+
+        status_font2 = self.font.render(
+            f"Top Light: {'Green' if self.is_green_top else 'Red'}   "
+            f"Bottom Light: {'Green' if self.is_green_bottom else 'Red'}",
+            True, (0, 0, 0)
+        )
+
         self.screen.blit(status_font, (10, 10))
+        self.screen.blit(status_font2, (10, 40))
 
     def draw_instructions(self):
         instructions_font = self.font.render(
@@ -90,6 +107,8 @@ class TrafficSimulation:
     def restart_game(self):
         self.lane1_cars = []
         self.lane2_cars = []
+        self.lane3_cars = []
+        self.lane4_cars = []
         print("Game restarted")
 
 
@@ -100,32 +119,52 @@ class TrafficSimulation:
             self.last_pause_toggle_time = current_time
             print("Pause/Resume toggled. Is paused:", self.is_paused)
             if self.is_paused:
-                self.game_clock.tick() 
+                self.game_clock.tick()
 
-    def draw_bridge(self, y):
-        pygame.draw.rect(self.screen, (128, 128, 128), (0, y, self.width, 100))
+    def draw_bridge(self, x, y, is_vertical=True):
+        if is_vertical:
+            pygame.draw.rect(self.screen, (128, 128, 128), (x, 0, 100, self.height))
+        else:
+            pygame.draw.rect(self.screen, (128, 128, 128), (0, y, self.width, 100))
 
     def move_cars(self):
         for car in self.lane1_cars:
-            if self.is_green_right:
+            if self.is_green_right or car not in self.lane1_cars[-3:]:
                 car.position -= car.speed
 
-            if not self.is_green_right and car.position < self.traffic_light_right.x :
+            if not self.is_green_right and car.position < self.traffic_light_right.x and car not in self.lane1_cars[
+                                                                                                    -3:]:
                 car.position -= car.speed
-                
 
         for car in self.lane2_cars:
-            if self.is_green_left:
-                car.position += car.speed
-            
-            if not self.is_green_left and car.position > self.traffic_light_left.x:
+            if self.is_green_left or car not in self.lane2_cars[-3:]:
                 car.position += car.speed
 
+            if not self.is_green_left and car.position > self.traffic_light_left.x and car not in self.lane2_cars[-3:]:
+                car.position += car.speed
+
+        # Mover los autos en la calle de arriba
+        for car in self.lane3_cars:
+            if self.is_green_bottom or car not in self.lane3_cars[-3:]:
+                car.position += car.speed
+
+            if not self.is_green_bottom and car.position < self.traffic_light_bottom.y and car not in self.lane3_cars[
+                                                                                                      -3:]:
+                car.position += car.speed
+
+        # Mover los autos en la calle de abajo
+        for car in self.lane4_cars:
+            if self.is_green_top or car not in self.lane4_cars[-3:]:
+                car.position -= car.speed
+
+            if not self.is_green_top and car.position > self.traffic_light_top.y and car not in self.lane4_cars[-3:]:
+                car.position -= car.speed
 
         # Actualizar las listas de acuerdo con las nuevas posiciones
         self.lane1_cars = [car for car in self.lane1_cars if car.position > 0]
-        self.lane2_cars = [car for car in self.lane2_cars if car.position < self.width]  # Mover el carro detenido al final de la cola
-
+        self.lane2_cars = [car for car in self.lane2_cars if car.position < self.width]
+        self.lane3_cars = [car for car in self.lane3_cars if car.position < self.height]
+        self.lane4_cars = [car for car in self.lane4_cars if car.position > 0]
 
     def update_traffic_lights(self):
         current_time = pygame.time.get_ticks()
@@ -138,20 +177,27 @@ class TrafficSimulation:
                 #pasa a rojo/rojo
                 self.is_green_left = False
                 self.is_green_right = False
-                self.semaphore_interval = 3000
+                self.is_green_bottom = True
+                self.is_green_top = False
+
+                self.semaphore_interval = 8000
                 self.fase = 2
             elif not self.is_green_left and  not self.is_green_right:
-                # Ambos rojos (3 segundos)
+                # Ambos rojos (5 segundos)
                 if self.fase == 2:
                     #pasa a rojo/verde
                     self.is_green_left = False
                     self.is_green_right = True
-                    self.semaphore_interval = 5000
+                    self.is_green_bottom = False
+                    self.is_green_top = False
+                    self.semaphore_interval = 8000
                     self.fase = 3
                 elif self.fase == 0:
                     #pasa a verde/rojo
                     self.is_green_left = True
                     self.is_green_right = False
+                    self.is_green_top = False
+                    self.is_green_bottom = False
                     self.semaphore_interval = 8000
                     self.fase = 1
             elif not self.is_green_left and self.is_green_right:
@@ -159,7 +205,8 @@ class TrafficSimulation:
                 #pasa a rojo/rojo
                 self.is_green_left = False
                 self.is_green_right = False
-                self.semaphore_interval = 3000
+                self.is_green_top = True
+                self.semaphore_interval = 8000
                 self.fase = 0
 
             # Reiniciar el tiempo restante
@@ -170,9 +217,9 @@ class TrafficSimulation:
 
             if self.remaining_semaphore_time <= 0:
                 self.remaining_semaphore_time = self.semaphore_interval
-                    
+
     def generate_cars(self):
-            
+
             min_distance = 50
 
             if not self.is_green_left and len(self.lane1_cars) < 3 and random.random() < self.car_generation_chance:
@@ -180,12 +227,26 @@ class TrafficSimulation:
                     car_lane1 = Car(1)
                     car_lane1.position = self.width
                     self.lane1_cars.append(car_lane1)
-    
+
             if not self.is_green_right and len(self.lane2_cars) < 5 and random.random() < self.car_generation_chance:
                 if not self.lane2_cars or self.lane2_cars[-1].position > min_distance:
                     car_lane2 = Car(2)
                     car_lane2.position = 0
                     self.lane2_cars.append(car_lane2)
+
+            #Generar autos en la calle de arriba
+            if not self.is_green_top and len(self.lane3_cars) < 3 and random.random() < self.car_generation_chance:
+                if not self.lane3_cars or self.lane3_cars[-1].position > min_distance:
+                    car_lane3 = Car(3)
+                    car_lane3.position = 0
+                    self.lane3_cars.append(car_lane3)
+
+            #Generar autos en la calle de abajo
+            if not self.is_green_bottom and len(self.lane4_cars) < 3 and random.random() < self.car_generation_chance:
+                if not self.lane4_cars or self.lane4_cars[-1].position < self.width - min_distance:
+                    car_lane4 = Car(4)
+                    car_lane4.position = self.width
+                    self.lane4_cars.append(car_lane4)
 
 
 
@@ -195,3 +256,11 @@ class TrafficSimulation:
 
         for car in self.lane2_cars:
             car.draw_car(self.screen, car.position, self.height // 3 + 50, 180)
+
+        # Dibuja los autos en la calle de arriba
+        for car in self.lane3_cars:
+            car.draw_car(self.screen, self.width // 3 + 120, car.position, 90)
+
+        # Dibuja los autos en la calle de abajo
+        for car in self.lane4_cars:
+            car.draw_car(self.screen, self.width // 3 + 190, car.position, 270)
